1e70ba27febf05140a790bb23278a885ec3e7058
Marco <markcostah@gmail.com>
2021-12-28 23:24:55 +0000

    project organization

--- Scripts/World_Objects/fallzone.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-func _on_Fallzone_body_entered(body):
-	if body.name == 'Player':
-		Global.player_dead = true
-		yield(get_tree().create_timer(1), "timeout")
-		get_tree().reload_current_scene()
